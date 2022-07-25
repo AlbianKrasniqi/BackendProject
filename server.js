2c2ac6b8cf125fa849b13968bae3d33e14f8bbbc
Albi <albikrasniqi31@gmail.com>
2022-07-25 18:42:45 +0200

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const app = require("./app");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  database: "backendproject",
+  user: "root",
+  password: "mysql1234",
+});
+
+connection.connect((error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("MySQL database is connected successfully!");
+});
 
 const port = process.env.PORT || 3000;
 const hostname = "127.0.0.1";
